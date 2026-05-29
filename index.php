83419275b1d6e07aa03d42c21ac29d8efeed459f
karinaaftrr <karinafitriamalia05@gmail.com>
2026-05-29 16:31:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+
+session_set_cookie_params([
+    'lifetime' => 300,
+    'path' => '/',
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
 session_start();
+
+include 'session_timeout.php';
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'landing';
 
@@ -9,6 +19,10 @@ switch($page){
 
     case 'landing':
         include 'views/landing_page.php';
+        break;
+
+    case 'detail_artikel':
+        include 'controllers/pengguna/detail_artikel.php';
         break;
 
     case 'login':
@@ -47,12 +61,12 @@ switch($page){
         include 'controllers/admin/hapus_user.php';
         break;
 
-    case 'jadwal':
-        include 'views/jadwal_posyandu.php';
-        break;
+    case 'riwayat_pemeriksa':
+         include 'controllers/pengguna/riwayat_pemeriksa.php';
+         break;
 
-    case 'riwayat':
-        include 'views/riwayat_pemeriksa.php';
+    case 'profile':
+        include 'controllers/pengguna/profile.php';
         break;
 
     case 'data_anak':
@@ -67,6 +81,10 @@ switch($page){
         include 'controllers/kader/data_balita.php';
     break;
 
+    case 'data_bumil':
+        include 'controllers/kader/data_bumil.php'; 
+    break;
+
     case 'jadwal_kader':
         include 'controllers/kader/jadwal_kader.php';
     break;
@@ -79,8 +97,44 @@ switch($page){
         include 'controllers/admin/jadwal_admin.php';
     break;
 
+    case 'daftar_balita':
+        include 'controllers/pengguna/daftar_balita.php';
+    break;
+
+    case 'daftar_bumil':
+        include 'controllers/pengguna/daftar_bumil.php';
+    break;
+
+    case 'histori_booking':
+        include 'controllers/pengguna/histori_booking.php';
+    break;
+
+    case 'lupa_password':
+        include 'controllers/auth/lupa_password.php';
+    break;
+    
+    case 'edukasi_kesehatan':
+        include 'controllers/pengguna/artikel.php';
+    break;
+
+    case 'pemeriksaan_balita':
+        include 'controllers/kader/pemeriksaan_balita.php';
+    break;
+
+    case 'pemeriksaan_bumil':
+        include 'controllers/kader/pemeriksaan_bumil.php';
+    break;
+
+    case 'logout':
+        include 'controllers/auth/logout.php';
+    break;
+
+    case 'artikel_edukasi':
+        include 'controllers/kader/artikel_edukasi.php';
+    break;
+    
     default:
         include 'views/landing_page.php';
-        break;
+    break;
 }
 ?>
