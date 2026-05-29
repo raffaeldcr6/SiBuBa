d922c72ce217a9455c131310f8efe295b6c9419b
raffaeldcr6 <raffaelramadhani250692@gmail.com>
2026-05-29 17:50:37 +0700

    Update sidebar_kader.php

--- views/components/sidebar_kader.php
+++ views/components/sidebar_kader.php
@@ -1,12 +1,16 @@
 <?php $current = isset($_GET['page']) ? $_GET['page'] : 'dashboard_kader'; ?>
 
-<button class="hamburger" id="adminHamburger">
-    <i class="fa-solid fa-bars" id="adminHamburgerIcon"></i>
+<button class="sidebar-toggle" id="hamburger" type="button">
+    <img
+    src="assets/icons/menu.svg"
+    class="icon-toggle-sidebar"
+    id="iconHamburger"
+    alt="Menu">
 </button>
 
-<div class="overlay" id="adminOverlay"></div>
+<div class="overlay" id="overlay"></div>
 
-<aside class="sidebar" id="adminSidebar">
+<aside class="sidebar" id="sidebar">
 
     <div class="sidebar-logo">
         <h2 class="logo-besar">SI BUBA</h2>
@@ -16,40 +20,41 @@
     <nav>
         <a href="index.php?page=dashboard_kader"
            class="menu-item <?= $current == 'dashboard_kader' ? 'aktif' : '' ?>">
-            <i class="fa-solid fa-house"></i>
+            <img src="assets/icons/dashboard.svg" class="icon-sidebar">
             Dashboard
         </a>
 
         <a href="index.php?page=data_balita"
            class="menu-item <?= $current == 'data_balita' ? 'aktif' : '' ?>">
-            <i class="fa-solid fa-users"></i>
+            <img src="assets/icons/child.svg" class="icon-sidebar">
             Data Balita
         </a>
 
         <a href="index.php?page=data_bumil"
            class="menu-item <?= $current == 'data_bumil' ? 'aktif' : '' ?>">
-            <i class="fa-solid fa-users"></i>
-            Data Ibu Hamil
+            <img src="assets/icons/pregnant.svg" class="icon-sidebar">
+            Data Bumil
         </a>
 
         <a href="index.php?page=jadwal_kader"
            class="menu-item <?= $current == 'jadwal_kader' ? 'aktif' : '' ?>">
-            <i class="fa-regular fa-calendar"></i>
-            Jadwal
+            <img src="assets/icons/calendar.svg" class="icon-sidebar">
+            Jadwal Posyandu
         </a>
 
         <a href="index.php?page=artikel_edukasi"
            class="menu-item <?= $current == 'artikel_edukasi' ? 'aktif' : '' ?>">
-            <i class="fa-regular fa-calendar"></i>
+            <img src="assets/icons/article.svg" class="icon-sidebar">
             Artikel Edukasi
         </a>
 
         
         <hr class="garis-menu">
 
-        <a href="controllers/logout.php" class="menu-item keluar">
-            <i class="fa-solid fa-right-from-bracket"></i>
-            Keluar
+        <a href="index.php?page=login_pengguna.php"
+        class="menu-item keluar">
+        <img src="assets/icons/logout.svg" class="icon-sidebar">
+        Keluar
         </a>
     </nav>
 
